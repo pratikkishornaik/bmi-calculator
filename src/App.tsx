9c608288e1edb4e3439e3b46be1964493e73f2cc
Pratik Naik <pratikkishornaik@gmail.com>
2022-01-05 02:16:39 +0530

    BMI calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BMIWidget from "./Component/BmiWidget";
+import "./App.css";
+import { useState, useRef } from "react";
+
+const initialState = {
+  weight: "",
+  height: "",
+};
 
 function App() {
+  const [formData, setFormData] = useState(initialState);
+  const [formError, setFormError] = useState(false);
+  const [bmiScore, setBmiScore] = useState(null);
+  const bmiWidget: any = useRef(null);
+  const bmiForm: any = useRef(null);
+
+  const calculateBMI = () => {
+    const { weight, height } = formData;
+    if (+weight > 0 && +height > 0) {
+      const result: any = Math.round((+weight / Math.pow(+height, 2)) * 10000);
+      setBmiScore(result);
+    }
+  };
+
+  const inputChange = (e: any) => {
+    const element = e.target;
+    setFormData({ ...formData, [element.name]: +element.value });
+  };
+
+  const validateForm = () => {
+    const { weight, height } = formData;
+
+    if ((!height && !weight) || +weight > 500 || +height > 250) {
+      setFormError(true);
+      return false;
+    }
+    setFormError(false);
+    return true;
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+    calculateBMI();
+    bmiWidget.current.scrollIntoView();
+  };
+
+  const recalculateBmi = () => {
+    bmiForm.current.scrollIntoView();
+    setFormData(initialState);
+    setBmiScore(null);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="containers" ref={bmiForm}>
+        <form className="form" onSubmit={handleSubmit}>
+          <h2>Human Body Mass Calculator</h2>
+          <h4>Calculate Your Body Mass Index</h4>
+          <input
+            type="number"
+            placeholder="Weight (kgs)"
+            name="weight"
+            onChange={inputChange}
+            value={formData.weight}
+            autoComplete="off"
+          />
+
+          <input
+            type="number"
+            placeholder="Height (cms)"
+            name="height"
+            onChange={inputChange}
+            value={formData.height}
+            autoComplete="off"
+          />
+          {formError && <div className="error">Please enter valid inputs</div>}
+          <button type="submit"> Calculate BMI</button>
+        </form>
+      </div>
+      <div className="containers" ref={bmiWidget}>
+        <BMIWidget score={bmiScore} />
+        <button type="button" onClick={recalculateBmi}>
+          Re-Calculate BMI
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/Component/BmiWidget/index.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import "./bmi.css";
+
+interface IScore {
+  max: number;
+  min: number;
+}
+
+const BMIWidget = (props: any) => {
+  const { score } = props;
+  const [bmiResult, setBmiResult] = useState("");
+  const [rotateDeg, setRotateDeg] = useState(5);
+
+  const DEGREE_CLASSIFICATION: any = {
+    UNDERWEIGHT: 32,
+    HEALTHY: 95,
+    OVERWEIGHT: 145,
+    OBESE: 173,
+  };
+
+  const BMI_CLASSIFICATION: any = {
+    UNDERWEIGHT: { max: 18, min: 0 },
+    HEALTHY: { max: 24, min: 19 },
+    OVERWEIGHT: { max: 29, min: 25 },
+    OBESE: { max: 150, min: 30 },
+  };
+
+  useEffect(() => {
+    if (score) {
+      calculateBmiRange();
+    } else {
+      setBmiResult("");
+      setRotateDeg(5);
+    }
+  }, [props.score]);
+
+  const calculateBmiRange = () => {
+    const bmiResult: any = Object.keys(BMI_CLASSIFICATION).find(
+      (key: any, index, arr) => {
+        const classifier: IScore = BMI_CLASSIFICATION[key];
+        if (score >= classifier.min && score <= classifier.max) {
+          return true;
+        }
+        if (index === arr.length - 1) {
+          if (score > classifier.max) {
+            return true;
+          }
+        }
+      }
+    );
+    setRotateDeg(DEGREE_CLASSIFICATION[bmiResult]);
+    setBmiResult(bmiResult || "");
+  };
+
+  const renderGaugeSeperator = () => {
+    const degrees = [329, 35, 63];
+    return degrees.map((deg) => {
+      return (
+        <div
+          key={deg + ""}
+          className="gauge_seperator"
+          style={{ transform: `rotate(${deg}deg)` }}
+        ></div>
+      );
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="gauge_body">
+        <div className="gauge_fill">
+          {renderGaugeSeperator()}
+          <div className="gauge_track">
+            <div>
+              <h2>{!score ? "Enter your details" : score}</h2>
+              <h4>{bmiResult}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="tracker_container fill"
+        style={{ transform: `rotate(${rotateDeg}deg)` }}
+      >
+        <div className="tracker "></div>
+      </div>
+    </div>
+  );
+};
+
+export default BMIWidget;
